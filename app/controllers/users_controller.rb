7da3950c19c99cb22a97adb53f5c25995d3c48ea
mika1201m <yuna1201m@gmail.com>
2024-08-11 19:59:29 +0900

    ログイン後トップページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to login_path
     else
       render :new
     end  
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, gender)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :gender)
   end
 
 end
