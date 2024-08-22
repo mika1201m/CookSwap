c8210452f9c8669681d5aa658d5f29cd092d80ed
mika1201m <yuna1201m@gmail.com>
2024-08-22 22:36:55 +0900

    デザインの変更

--- app/models/recipe_material.rb
+++ app/models/recipe_material.rb
@@ -1,6 +1,6 @@
 class RecipeMaterial < ApplicationRecord
     validates :volume, presence: true
-    validates :scale, presence: true, length: { maximum: 10 }, uniqueness: true
+    validates :scale, presence: true, length: { maximum: 10 }
 
     belongs_to :material
     belongs_to :recipe
